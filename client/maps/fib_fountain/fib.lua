1583b48c004f7efc2118657f50b916a5a03b86a0
TayMcKenzieSA <taylorocarroll@gmail.com>
2022-03-09 03:34:22 +1300

    Update fib.lua

--- client/maps/fib_fountain/fib.lua
+++ client/maps/fib_fountain/fib.lua
@@ -7,6 +7,10 @@ Citizen.CreateThread(function()
     -- RemoveIpl("dt1_05_hc_req")
     RequestIpl("dt1_05_hc_remove")
     -- RemoveIpl("dt1_05_rubble")
+	
+	
+    -- FIB Rooftop Glass: 134.33, -745.95, 266.98
+    RequestIpl("atriumglmission")
 
 
 end)
